c5363d09b2fe879161f622bdba30ffb7737e8d0c
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-30 09:46:03 +0000

    Create Sor.java

---
+++ Sor.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Sor
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+         int n=in.nextInt();
+         int[] a=new int[n];
+         for(int i=0;i<n;i++)
+         {
+             a[i]=in.nextInt();
+         }
+         Arrays.sort(a);
+         for(int i=0;i<n;i++)
+         {
+         System.out.print(a[i]+" ");
+         }
+    }
+}
