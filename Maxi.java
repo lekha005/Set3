4a10827ab866f4b9cc245fc6c458d3869f0d9725
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-30 09:42:44 +0000

    Create Maxi.java

---
+++ Maxi.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Maxi
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+         int n=in.nextInt();
+         int[] a=new int[n];
+         for(int i=0;i<n;i++)
+         {
+             a[i]=in.nextInt();
+         }
+         Arrays.sort(a);
+         System.out.print(a[n-1]);
+    }
+}
